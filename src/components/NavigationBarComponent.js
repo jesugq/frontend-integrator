5a8e7829d1c3aa3f804668f374f27b69cbcb708f
jesugq <jesugq@gmail.com>
2019-08-31 15:00:40 -0500

    Add Navigation Bar Component

---
+++ src/components/NavigationBarComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavigationBar extends Component {
+    // The Navigation Bar disappears when scrolling downwards and viceversa.
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default NavigationBar;
