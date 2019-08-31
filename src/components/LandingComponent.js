76f151982b4f36849730fcc72c89be0528c43b9d
jesugq <jesugq@gmail.com>
2019-08-31 14:48:55 -0500

    Add Landing Component

---
+++ src/components/LandingComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    /* Calls upon the other eight components of the landing page.
+        1. Navigation Bar Component
+        2. Search Bar Component
+        3. Qualities Component
+        4. Locations Component
+        5. Subjects Component
+        6. Register Component
+        7. About Us Component
+        8. Copyright Component
+    These names are merely suggestions, feel free to follow your own
+     naming conventions. */
+    render (){
+        return(
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Landing;
