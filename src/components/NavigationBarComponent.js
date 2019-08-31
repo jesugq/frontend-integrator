184dcddfdbf9cd19f3bbfee4185cfb1eb6c3b2cb
jesugq <jesugq@gmail.com>
2019-08-31 15:24:17 -0500

    Add Hide on Scroll functionality to Navigation Bar Component

--- src/components/NavigationBarComponent.js
+++ src/components/NavigationBarComponent.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
+import classnames from "classnames"
 
 class NavigationBar extends Component {
+    // State holds the vertical position of the website for usage in hiding.
+    constructor() {
+        super(props);
+
+        this.state = {
+            previousOffset: window.pageYOffset,
+            visible: true
+        }
+    }
+
+    // Adds and Removes the Scroll Event Listener.
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    // Handles Scroll Action for usage in hiding.
+    handleScroll() {
+        const { previousOffset } = this.state;
+        const currentOffset = window.pageYOffset;
+        const visible       = previousOffset > currentOffset;
+
+        this.setState({
+            previousOffset: currentOffset,
+            visible
+        });
+    }
+    
     // The Navigation Bar disappears when scrolling downwards and viceversa.
     render() {
         return (
