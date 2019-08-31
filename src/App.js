f25d3011b415e8056db6dd9920e0577499f3a3d3
jesugq <jesugq@gmail.com>
2019-08-31 14:51:42 -0500

    Add Landing Component to App file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Landing from './components/LandingComponent'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Landing />
     </div>
   );
 }
