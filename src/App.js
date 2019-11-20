be2748dc2adf1b1fcb50361444b2f5130ed59914
Pantuflas <antoniojss94@gmail.com>
2019-11-20 11:41:16 -0600

    side menu created not functional yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import NavigationComponent from './components/global/NavigationComponent';
+import SideMenuComponent from './components/global/SideMenuComponent';
 
 import 'mdbreact/dist/css/mdb.css'
 import './App.css';
 
 function App() {
   return (
-    <NavigationComponent />
+
+    <div>
+      <NavigationComponent />
+      <SideMenuComponent />
+    </div>
+   
   );
 }
 
